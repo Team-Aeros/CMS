0373b4c025654d6afbab0779f915c2fdb0a38802
Robert Monden <dev@robertmonden.com>
2018-07-14 13:35:10 +0200

    + Create autoloader

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WIP CMS
+ * Open source content management system
+ *
+ * @version 1.0 Alpha 1
+ * @author Aeros Development
+ * @copyright 2018, WIP CMS
+ * @link https://aeros.com/wipcms
+ *
+ * @license MIT
+ */
+
+spl_autoload_register(function(string $classname) : void {
+    $parts = explode('\\', $classname);
+
+    if ($parts[0] === 'WIPCMS')
+        unset($parts[0]);
+
+    $filename = implode('/', $parts);
+
+    // Load the file if we can. If not, it's not our problem.
+    if (file_exists($location = __DIR__ . '/' . $filename . '.class.php'))
+        require $location;
+    else if (file_exists($location = __DIR__ . '/' . $filename . '.interface.php'))
+        require $location;
+});
