2ec9472036f5f62234bc1d9c45839cfd49ffb212
jhhaanstra <janhendrikhaanstra@gmail.com>
2018-07-16 14:41:52 +0200

    ORM + migrations working

---
+++ core/database/ORM.class.php
@@ -0,0 +1,76 @@
+<?php
+namespace WIPCMS\core\database;
+
+use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console\Helper\HelperSet;
+
+class ORM {
+    private static $_instance;
+    private $paths;
+    private $isDevMode;
+    private $dbParams;
+    private $config;
+    private $entityManager;
+
+    private function __construct()
+    {
+        $this->paths = ['../entities'];
+        $this->isDevMode = true;
+
+        $this->dbParams = include 'migrations-db.php';
+
+        $this->config = Setup::createAnnotationMetadataConfiguration($this->paths, $this->isDevMode);
+        $this->entityManager = EntityManager::create($this->dbParams, $this->config);
+    }
+
+    public static function getInstance() : ORM
+    {
+        if (self::$_instance == null)
+            self::$_instance = new self();
+
+        return self::$_instance;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPaths()
+    {
+        return $this->paths;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDevMode()
+    {
+        return $this->isDevMode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDbParams()
+    {
+        return $this->dbParams;
+    }
+
+    /**
+     * @return \Doctrine\ORM\Configuration
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+}
---
+++ migrations-db.php
@@ -0,0 +1,11 @@
+<?php
+//  https://www.doctrine-project.org/projects/doctrine-migrations/en/latest/reference/introduction.html#introduction
+
+
+return [
+    'dbname' => 'cms',
+    'user' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+];
