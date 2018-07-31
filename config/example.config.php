9d2f87769a6785d31c24114a78fe480bfdd1680a
jhhaanstra <janhendrikhaanstra@gmail.com>
2018-08-01 00:57:18 +0200

    example.config.php returned

---
+++ config/example.config.php
@@ -0,0 +1,15 @@
+<?php
+
+const CONFIG = [
+    'database' => [
+        'dbname' => 'cms',
+        'user' => 'root',
+        'password' => 'root',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql',
+    ],
+
+    'site' => [
+        'default_language' => 'en_US'
+    ]
+];
